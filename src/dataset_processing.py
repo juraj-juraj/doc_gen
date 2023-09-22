27e62d13d46ac5b5b4e72fdfa3dcf8bed4f567ab
Juraj Novosad <juraj.novosad.sk@gmail.com>
2023-09-22 13:04:07 +0200

    make better dataset script

--- src/dataset_processing.py
+++ src/dataset_processing.py
@@ -1,8 +1,16 @@
+#################
+# Dataset processing script
+# Process data to huggingface dataset
+# Remove docstring from code examples and remove those without documentation
+# Dataset downloaded from https://www.kaggle.com/datasets/omduggineni/codesearchnet?resource=download
+###########
+
 import argparse
-import pickle
 import re
 
 import pandas as pd
+from datasets import Dataset, DatasetDict
+from tqdm import tqdm
 
 
 def parse_arguments() -> dict:
@@ -17,33 +25,56 @@ def parse_arguments() -> dict:
     parser = argparse.ArgumentParser(prog="dataset_processing ")
     parser.add_argument("-f", "--file", type=str, help="pickle dataset to process")
     parser.add_argument(
-        "-o", "--output", type=str, default="stdout", help="File to save processed dateset"
+        "--output_file",
+        type=str,
+        default=None,
+        help="File to save processed dateset",
+    )
+    parser.add_argument(
+        "--repository", type=str, default=None, help="Repository to push dataset to"
     )
     return parser.parse_args()
 
 
 def remove_docstring(function_string: str) -> str:
+    """Function to strip python function of docstring
+
+    Input:
+        function_string: Function with docstring
+    Returns:
+        str: Function without docstring
+    """
     return re.sub(r'("""|\'\'\')(.*?)(\1)', "", function_string, flags=re.DOTALL)
 
 
-def dumps_series(serie: pd.Series) -> str:
-    for fce in serie:
-        print(f"\n-----------\n {fce}")
-
-
 def main(arguments: dict):
+    bar = tqdm(range(6))
     raw_data = pd.read_pickle(arguments.file)
+    bar.update(1)
     df = pd.DataFrame(raw_data)
+    bar.update(1)
     df = df[["docstring", "function"]].copy()
 
     df["function"] = df["function"].apply(remove_docstring)
     df = df[df["docstring"].str.len() > 0]
-
-    if arguments.output != "stdout":
-        with open(arguments.output, "wb") as f:
-            pickle.dump(df, f, protocol=pickle.HIGHEST_PROTOCOL)
-    else:
-        print(df.to_csv(index=True))
+    bar.update(1)
+    ds_test = Dataset.from_pandas(df.loc[0:1000])
+    ds_validation = Dataset.from_pandas(df.loc[1000:2000])
+    ds_train = Dataset.from_pandas(df.loc[2000:])
+    bar.update(1)
+    dataset_dict = DatasetDict(
+        {
+            "train": ds_train,
+            "validation": ds_validation,
+            "test": ds_test,
+        }
+    )
+    bar.update(1)
+    if arguments.output_file:
+        dataset_dict.save_to_disk(arguments.output_file)
+    if arguments.repository:
+        dataset_dict.push_to_hub(arguments.repository)
+    bar.update(1)
 
 
 if __name__ == "__main__":
