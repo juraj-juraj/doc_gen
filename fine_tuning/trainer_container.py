d5db6b457bab253ba902e3d76186e223a70e6027
Juraj Novosad <juraj.novosad.sk@gmail.com>
2024-03-26 23:27:06 +0100

    add embedding metric

--- fine_tuning/trainer_container.py
+++ fine_tuning/trainer_container.py
@@ -5,10 +5,10 @@ from typing import Callable, Literal
 
 import evaluate
 import numpy as np
+import sentence_transformers
 from datasets import Dataset, DatasetDict
 from fine_tuning_utils import postprocess_text
 from model_args import DataTrainingArguments
-from optimum.onnxruntime import ORTTrainer, ORTTrainingArguments
 from trainer_stat_collector import (
     StatCollectorI,
     dict_2_md_json,
@@ -56,6 +56,29 @@ def sacrebleu_metrics(tokenizer) -> Callable:
         result["gen_len"] = np.mean(prediction_lens)
         result = {k: round(v, 4) for k, v in result.items()}
         return result
+
+    return wrapper
+
+
+def embedding_metric(tokenizer, model: str = "all-MiniLM-L6-v2", device: str = "cuda") -> Callable:
+    metric = sentence_transformers.SentenceTransformer(model, device=device)
+
+    def wrapper(eval_preds: EvalPrediction):
+        preds, labels = eval_preds
+        if isinstance(preds, tuple):
+            preds = preds[0]
+        # Replace -100s used for padding as we can't decode them
+        # preds = np.where(preds != -100, preds, tokenizer.pad_token_id)
+        decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+        # labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+        # Some simple post-processing
+        decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+        preds_embeddings = metric.encode(decoded_preds, device=device)
+        refs_embeddings = metric.encode(decoded_labels, device=device)
+        score = sentence_transformers.util.pairwise_cos_sim(preds_embeddings, refs_embeddings).mean()
+        return {"score": score}
 
     return wrapper
 
